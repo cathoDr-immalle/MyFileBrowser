51b57fcdbac8529ac0995f851bf851f94b5abe7f
cathoDr-immalle <catho.driesen@immalle.be>
2017-10-27 11:23:16 +0200

    Methodes en switch ccases

--- MyFileBrowser/Program.cs
+++ MyFileBrowser/Program.cs
@@ -9,6 +9,58 @@ namespace MyFileBrowser
     {
         static void Main(string[] args)
         { 
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Geef een argument!");
+            }
+            else
+            {
+                switch(args[0])
+                {
+                    case "-c":
+                        ShowCurrentDirectory();
+                        break;
+                    case "-f":
+                        ShowAllFilesCurrentDirecty();
+                        break;
+                    case "-d":
+                        ShowSubdirectoryCurerntFile();
+                        break;
+                    default:
+                        UnknownArgument();
+                        break;
+                    case "-h":
+                        ShowHelp();
+                        break;
+                    
+                }
+            }
         }
+
+        static void ShowCurrentDirectory()
+        {
+            Console.WriteLine("Current directory");
+        }
+
+        static void ShowAllFilesCurrentDirecty()
+        {
+            Console.WriteLine("All files in current directory");
+        }
+
+        static void ShowSubdirectoryCurerntFile()
+        {
+            Console.WriteLine("subdirectory in current file");
+        }
+
+        static void UnknownArgument()
+        {
+            Console.WriteLine("Unknown argument");
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine("Show help");
+        }
+
     }
 }
